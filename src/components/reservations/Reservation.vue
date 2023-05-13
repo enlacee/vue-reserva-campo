60a3c8f3e6f3245a9b5f7f0d3e8e6485baa0648e
Anibal Copitan <github@pprios.com>
2023-05-13 18:45:02 -0500

    [add] Adding data JSON using APPSCRIPT + SpreadSheet

--- src/components/reservations/Reservation.vue
+++ src/components/reservations/Reservation.vue
@@ -1,9 +1,19 @@
 <script>
+// Crear un mapero del objeto client:
+// para setearlo bien
+// Usar typescript lo valida mejor
 export default {
   data() {
     return {
       clientid: '',
+      client: {
+        'nombre-campo': ''
+      }
     }
+  },
+  mounted() {
+    this.loadCliendId();
+    this.testingLoadClientData();
   },
   computed: {},
   methods: {
@@ -23,11 +33,19 @@ export default {
         clientid = urlParams.get('clientid');
         this.clientid = clientid;
       }
+    },
+    testingLoadClientData() {
+      const URLFATHER = 'https://script.google.com/macros/s/AKfycbzSyhh6sM_shxp9Jy8qa0aDx-08C7XT-CxOuV_pBGzUovrngM0TfOdhzw94TVIDcnXE/exec';
+      const URL = URLFATHER + '?op=list';
+      fetch(URL, {method: 'GET'})
+      .then(response => response.json())
+      .then((data) => {
+        console.log('response', data, data[0]['cliente']);
+        this.client = data[0];
+      });
     }
   },
-  mounted() {
-    this.loadCliendId();
-  }
+
 }
 </script>
 <script setup>
@@ -42,7 +60,7 @@ import PriceTable from '../reservations/objects/PriceTable.vue';
       <ImageSoccerBall />
     </div>
     <div class="w-full">
-      <h1 class="text-center font-bold">{{ clientid }}</h1>
+      <h1 class="text-center font-bold">{{ clientid }} {{ client['cliente'] }}</h1>
     </div>
     <div class="w-full">
       <form class="bg-white px-8 pt-6 pb-8 mb-4">
@@ -51,17 +69,19 @@ import PriceTable from '../reservations/objects/PriceTable.vue';
             Campo Deportivo
           </label>
           <input class="shadow appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="username" type="text" placeholder=""
-            value="Sarita Colonia Hijos de Villa los Reyes">
+            id="username" type="text"
+            placeholder="Sarita Colonia Hijos de Villa los Reyes" :value="client['nombre-campo']">
         </div>
         <div class="mb-4">
           <label class="block text-gray-700 text-sm mb-2" for="password">
             Direccion
           </label>
           <input class="shadow appearance-none border w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-            id="password" type="text" placeholder="" value="Lizardo monetero #843 Ventanilla-Callao">
+            id="password" type="text" placeholder="Lizardo monetero #843 Ventanilla-Callao" 
+              :value="client['direccion']">
         </div>
         <div class="mb-6 flex place-content-center">
+          <!-- pasarle data a esta tabla -->
           <PriceTable />
         </div>
         <div class="flex items-center justify-between">
