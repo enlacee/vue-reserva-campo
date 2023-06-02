666fc67d4fab61442f64331091f06b8f139002f7
Anibal Copitan <github@pprios.com>
2023-06-01 19:22:15 -0500

    [add] easy select the time

--- src/components/reservations/steps/TwoTime.vue
+++ src/components/reservations/steps/TwoTime.vue
@@ -3,12 +3,42 @@ export default {
   emits: ['changeCurrentComponent'],
   data() {
     return {
+      selectedClass: false
     }
   },
   computed: {
     isCurrentHour() {
       return this.selectedYear == new Date().getHours();
     },
+  },
+  methods: {
+    select: function(event){
+      if (event) {
+        // 01. Controll number to clicked
+        let allowedNumbers = [];
+        let selectedButtons = document.querySelectorAll('#button-container button.selected');
+        if (selectedButtons.length > 0) {
+          for (let index = 0; index < selectedButtons.length; index++) {
+            const theNumber = parseInt(selectedButtons[index].getAttribute('aria-label'));
+            allowedNumbers.push(theNumber);
+          }
+          // Adding Minor and Mayor range
+          const first = allowedNumbers[0];
+          const last = allowedNumbers[allowedNumbers.length - 1];
+          allowedNumbers.push(first-1);
+          allowedNumbers.push(last+1);
+        }
+        console.log('allowedNumbers 0', allowedNumbers);
+
+        // 02. apply clicked with filter
+        let numberSelected = parseInt(event.target.getAttribute('aria-label'));
+        if (allowedNumbers.length === 0 || allowedNumbers.includes(numberSelected) === true) {
+          let el = document.querySelector(`button[aria-label="${numberSelected}"]`);
+          el.classList.toggle("selected");
+        }
+        console.log('allowedNumbers 1', allowedNumbers);
+      }
+    }
   }
 }
 
@@ -18,14 +48,16 @@ export default {
     <div class="w-full">
       <h1 class="text-center font-bold text-xl mb-6">Seleccione la hora(s) disponible(s) [24horas]</h1>
     </div>
-    <div class="flex-auto">
+    <div class="flex-auto" id="button-container">
       <button v-for="n in 24" :disabled="(n < new Date().getHours()) ? true : false"
         class="bg-white text-black py-2 px-4 border-black border w-1/5 mr-1 mb-1 border border-gray-400 shadow"
-        :class="(n < new Date().getHours()) ? 'disabled:opacity-75 cursor-not-allowed' : 'hover:bg-gray-100'">
+        @click="select($event)"
+        :aria-label="n">
         {{ n }}
       </button>
     </div>
     <p>&nbsp;</p>
+
     <div class="flex gap-2">
       <button
         @click="$emit('changeCurrentComponent', 'OneDate')"
@@ -40,3 +72,11 @@ export default {
     </div>
   </div>
 </template>
+<style scoped>
+  button[disabled]{
+    opacity: 0.5;
+  }
+  .selected{
+    background-color: #ccc;
+  }
+</style>
